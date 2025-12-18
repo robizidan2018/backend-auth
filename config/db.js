9d1805e039a99db0b3ee0b77b91c683b609ba04f
RobbyRZA <robizidan4@gmail.com>
2025-12-18 09:11:58 +0700

    membetulkan lagi dan lagi sampai gila

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   host: process.env.PGHOST,
-  port: process.env.PGPORT,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
   ssl: { rejectUnauthorized: false }
 });
 
